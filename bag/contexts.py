2a15a1f67971ca2bb22218dfe572ec7159ac6132
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-04-03 13:03:01 +0000

    Updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,10 +9,25 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
+    # Accessing the shopping bag
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    # if products total in bag is under $50. Delivery cost will be 10%
+    # of bag total
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    # if products total in bag is over $50, delivery is free
     else:
         delivery = 0
         free_delivery_delta = 0
