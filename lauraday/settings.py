7ac0d8d5348e0304c3f53bebe61b6d918e1a0000
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-04-04 16:53:43 +0000

    Added crispy forms fields for Details and Delivery

--- lauraday/settings.py
+++ lauraday/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'lauraday.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -72,6 +75,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_fields',
+            ]
         },
     },
 ]
