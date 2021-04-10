5b4f986ed670cc2a7bd86fe36b81b55f698fbe36
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-04-10 14:05:24 +0000

    Added billing details and shipping to form submit

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -52,13 +52,38 @@ form.addEventListener('submit', function(ev) {
     // To prevent multiple submissions
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    // Payment loading spinner
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
     // Confirm the card payment
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
-        }
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    state: $.trim(form.county.value),
+                }
+            }
+        },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                postal_code: $.trim(form.postcode.value),
+                state: $.trim(form.county.value), 
+            }
+        },
     // Execute result function
     }).then(function(result) {
         if (result.error) {
