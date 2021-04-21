28c64800b8f649c9fb0f48ff0eec88ea82338bcd
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-04-21 11:06:26 +0000

    Added database url for os environ, allowed hosts, procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lauraday.wsgi:application
--- lauraday/settings.py
+++ lauraday/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '3i)^b@zag&))=%j+@#6i2$(2*u_kx-z)z1qh+@-vz+57#znum%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lauraday-josefinekihlstrom.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'lauraday.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
