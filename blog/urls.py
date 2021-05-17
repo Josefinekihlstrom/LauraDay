31d8571b11c523ef05b23a069d9de1113dd4b0ff
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-05-17 16:45:40 +0000

    Added add post template, url, forms and view

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
+    path('add_post/', views.AddPost.as_view(), name='add_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.views import generic
 from .models import Post
+from .forms import BlogForm
+
 
 class PostList(generic.ListView):
     # Only posts with status published will show
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
 
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+# Add Blog Post code with help from Codemy.com
+# https://www.youtube.com/watch?v=m3efqF9abyg&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=4
+class AddPost(generic.CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
