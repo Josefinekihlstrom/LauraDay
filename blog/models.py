d722d738f96228a2ce72e7e0b65bb35ec5865a74
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-05-28 13:27:26 +0000

    Added comments model for blog app

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete= models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -25,3 +25,15 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.slug)])
+
+
+# Blog comments with the help from Codemy.com
+# https://www.youtube.com/watch?v=hZrlh4qU4eQ&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=34
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete= models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
