3651b4c6b03f5122af57a2cbe370920296fc0fa5
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-06-02 18:37:07 +0000

    Added comments

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,18 @@ from django.urls import reverse_lazy
 # https://djangocentral.com/building-a-blog-application-with-django/
 # https://www.youtube.com/watch?v=B40bteAMM_M&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=1
 class PostList(generic.ListView):
+    """ 
+    View that renders blog posts
+    """
     # Only posts with status published will show
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
 
 
 class PostDetail(generic.DetailView):
+    """ 
+    View that renders blog post details
+    """
     model = Post
     template_name = 'post_detail.html'
 
@@ -21,6 +27,9 @@ class PostDetail(generic.DetailView):
 # Add Blog Post code with help from Codemy.com
 # https://www.youtube.com/watch?v=m3efqF9abyg&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=4
 class AddPost(generic.CreateView):
+    """ 
+    Lets a superuser add a blog post
+    """
     model = Post
     template_name = 'add_post.html'
     fields = '__all__'
@@ -29,6 +38,9 @@ class AddPost(generic.CreateView):
 # Add comment code with help from Codemy.com
 # https://www.youtube.com/watch?v=OuOB9ADT_bo&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=35
 class AddComment(generic.CreateView):
+    """ 
+    Lets users add a comment to blog posts
+    """
     model = Comment
     form_class = AddCommentForm
     template_name = 'add_comment.html'
@@ -39,12 +51,16 @@ class AddComment(generic.CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
+        # returns the user back to the post details page after submitting the comment.
         return reverse_lazy('post_detail', kwargs={'slug': self.kwargs['slug']})
 
 
 # Edit Blog Post code with help from Codemy.com
 # https://www.youtube.com/watch?v=J7xaESAddDQ&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=6
 class EditPost(generic.UpdateView):
+    """ 
+    Lets a superuser edit blog posts
+    """
     model = Post
     template_name = 'edit_post.html'
     fields = ['title', 'slug', 'content']
@@ -53,6 +69,9 @@ class EditPost(generic.UpdateView):
 # Delete Blog Post with help from Codemy.com
 # https://www.youtube.com/watch?v=8NPOwmtupiI&list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi&index=7
 class DeletePost(generic.DeleteView):
+    """ 
+    Lets a superuser delete blog posts
+    """
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('blog')
