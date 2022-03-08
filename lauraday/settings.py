05ea0353ec6d3b2c3beff4d4ec59a3844c9c9954
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2022-03-08 16:18:04 +0000

    added staticfiles_storage

--- lauraday/settings.py
+++ lauraday/settings.py
@@ -178,6 +178,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
